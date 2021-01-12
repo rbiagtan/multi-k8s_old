8897ef97ba3145b7bee88c548a1ed18f85178b49
rbiagtan <rbiagtan@gmail.com>
2021-01-11 20:44:17 -0800

    initial k8s sync

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Home Modified Jan. 5 at 11:45 AM</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
