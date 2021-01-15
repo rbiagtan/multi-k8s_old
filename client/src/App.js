7758abb4b29681d3208bcbef51d7c3cbb2dd3be6
rbiagtan <rbiagtan@gmail.com>
2021-01-14 16:06:08 -0800

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Welcome to Kubernetes!!!
+            Welcome to Kubernetes
           </a>
           <Link to="/">Home Modified Jan. 5 at 11:45 AM</Link>
           <Link to="/otherpage">Other Page</Link>
